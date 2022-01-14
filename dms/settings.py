d04f1c8b1f2c8efe5294e08f9ef41d5726aed1ea
ramesh960386 <ramesh.daywell@gmail.com>
2022-01-14 17:45:36 +0530

    fine

--- dms/settings.py
+++ dms/settings.py
@@ -166,8 +166,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
